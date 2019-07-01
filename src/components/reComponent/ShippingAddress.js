56472af3606cdb00b04d20101e8ef0fe98b35d94
rwaidaalmehanni <rwaida4@gmail.com>
2019-07-01 14:27:17 +0300

    added update address function

--- src/components/reComponent/ShippingAddress.js
+++ src/components/reComponent/ShippingAddress.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Typography, TextField, FormControl
-  ,DialogContent, withStyles} from "@material-ui/core";
+import {
+  Typography, TextField, FormControl
+  , DialogContent, withStyles, Button
+} from "@material-ui/core";
 import Select from "@material-ui/core/Select/Select";
 import MenuItem from "@material-ui/core/MenuItem/MenuItem";
 // import { URLS } from "../../utils";
@@ -45,82 +47,110 @@ const styles = theme => ({
 
 });
 
+const api = "https://backendapi.turing.com/";
+
 class ShippingAddress extends Component {
   constructor() {
     super();
     this.state = {
+      address1: "",
+      address2: "",
+      city: "",
+      region: "",
+      postalCode: "",
+      country: null,
+      shippingRegion: null,
+      shippingOptions: null
     }
+    this._onSubmit = this._onSubmit.bind(this)
+  }
+
+  _onSubmit(){
+    let self = this;
+    $.ajax({
+      url: api + "customers/address",
+      type: "PUT",
+      data: this.state,
+      success: function(results) { self.setState({totalPrice:results.total_amount}) },
+      error: function(error) { console.log('errror',error); }
+    });
   }
 
   render() {
     const { classes } = this.props;
+    const { address1, address2, city, region, postalCode, country, shippingRegion, shippingOptions } = this.state;
     return (
+      <div>
+        <FormControl fullWidth>
+          <TextField label={'Address 1'} value={address1}
+                     onChange={(event) => {
+                       this.setState({address1: event.target.value})
+                     }}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <TextField label={'Address 2'} value={address2}
+                     onChange={(event) => {
+                       this.setState({address2: event.target.value})
+                     }}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <div className={classes.flexDiv}>
+            <div className={classes.shippingDiv}>
+              <TextField label={'City'} value={city} type={'text'}
+                         onChange={(event) => {
+                           this.setState({city: event.target.value})
+                         }}
+              />
+            </div>
+            <div className={classes.shippingDiv}>
+              <TextField label={'Region'} value={region} type={'text'}
+                         onChange={(event) => {
+                           this.setState({region: event.target.value})
+                         }}
+              />
+            </div>
+            <div className={classes.shippingDiv}>
+              <TextField label={'Postal Code'} value={postalCode} type={'text'}
+                         onChange={(event) => {
+                           this.setState({postalCode: event.target.value})
+                         }}
+              />
+            </div>
+          </div>
+        </FormControl>
+        <FormControl fullWidth>
+          <div className={classes.flexDiv}>
+            <div className={classes.shippingDiv}>
+              <Typography variant={'subtitle1'}> Country</Typography>
+              <Select value={country} onChange={(e)=>{this.setState({country: e.target.value})}} fullWidth>
+                <MenuItem children={'test1'} value={1} id={1}/>
+                <MenuItem children={'test2'} value={2} id={2}/>
+              </Select>
+            </div>
+            <div className={classes.shippingDiv}>
+              <Typography variant={'subtitle1'}> Shipping Region</Typography>
+              <Select value={shippingRegion} onChange={(e)=>{this.setState({shippingRegion: e.target.value})}} fullWidth>
+                <MenuItem children={'test1'} value={1} id={1}/>
+                <MenuItem children={'test2'} value={2} id={2}/>
+              </Select>
+            </div>
+            <div className={classes.shippingDiv}>
+              <Typography variant={'subtitle1'}> Shipping Options</Typography>
+              <Select value={shippingOptions} onChange={(e)=>{this.setState({shippingOptions: e.target.value})}} fullWidth>
+                <MenuItem children={'test1'} value={1} id={1}/>
+                <MenuItem children={'test2'} value={2} id={2}/>
+              </Select>
+            </div>
+          </div>
+        </FormControl>
         <div>
-          <FormControl fullWidth>
-            <TextField label={'Address 1'} value={null}
-                       onChange={(event) => {
-                         this.setState({address1: event.target.value})
-                       }}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <TextField label={'Address 2'} value={null}
-                       onChange={(event) => {
-                         this.setState({address2: event.target.value})
-                       }}
-            />
-          </FormControl>
-          <FormControl fullWidth>
-            <div className={classes.flexDiv}>
-              <div className={classes.shippingDiv}>
-                <TextField label={'City'} value={null} type={'password'}
-                           onChange={(event) => {
-                             this.setState({city: event.target.value})
-                           }}
-                />
-              </div>
-              <div className={classes.shippingDiv}>
-                <TextField label={'Region'} value={null} type={'password'}
-                           onChange={(event) => {
-                             this.setState({region: event.target.value})
-                           }}
-                />
-              </div>
-              <div className={classes.shippingDiv}>
-                <TextField label={'Postal Code'} value={null} type={'password'}
-                           onChange={(event) => {
-                             this.setState({postalCode: event.target.value})
-                           }}
-                />
-              </div>
-            </div>
-          </FormControl>
-          <FormControl fullWidth>
-            <div className={classes.flexDiv}>
-              <div className={classes.shippingDiv}>
-                <Typography variant={'subtitle1'}> Country</Typography>
-                <Select value={null} onChange={(e)=>{console.log('hello')}} fullWidth>
-                  <MenuItem children={'test1'} value={1} id={1}/>
-                  <MenuItem children={'test2'} value={2} id={2}/>
-                </Select>
-              </div>
-              <div className={classes.shippingDiv}>
-                <Typography variant={'subtitle1'}> Shipping Region</Typography>
-                <Select value={null} onChange={(e)=>{console.log('hello')}} fullWidth>
-                  <MenuItem children={'test1'} value={1} id={1}/>
-                  <MenuItem children={'test2'} value={2} id={2}/>
-                </Select>
-              </div>
-              <div className={classes.shippingDiv}>
-                <Typography variant={'subtitle1'}> Shipping Options</Typography>
-                <Select value={null} onChange={(e)=>{console.log('hello')}} fullWidth>
-                  <MenuItem children={'test1'} value={1} id={1}/>
-                  <MenuItem children={'test2'} value={2} id={2}/>
-                </Select>
-              </div>
-            </div>
-          </FormControl>
+          <Button children={<b>Submit Address</b>} variant={"contained"} style={{backgroundColor: '#6eb2fb',margin:'1em'}}
+                  onClick={()=>{this._onSubmit()}} type="submit"
+          />
         </div>
+      </div>
     );
   }
 }
